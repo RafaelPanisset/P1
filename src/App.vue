55c018acf9596273a68f1f7be454850b2ad49c00
RafaelPanisset <rafaelpani99@gmail.com>
2022-04-12 20:18:10 -0300

    nv

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/users">UserCrud</router-link>
+    <!--  <router-link to="/users">UserCrud</router-link>-->
       <router-link to="/carros"> CarrosCrud</router-link>
     </div>
     <div data-app>
